ec2f973e2a85582d66f7870206e56ab672877be9
@_@/ <57321062+LexxaPressF@users.noreply.github.com>
2020-02-16 22:38:10 +0300

    1 лаба не до конца

---
+++ Labs/1/4 ex/Divider/Divider/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Divider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Please enter the first integer");
+        string temp = Console.ReadLine();
+        }
+    }
+}
