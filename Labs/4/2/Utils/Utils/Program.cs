c22ff74e569408220895921eba0ec82fb065f5d5
LexxaPressF <malyshevalexey6@gmail.com>
2020-03-07 22:43:11 +0300

    4 лаба 2 упражнение

---
+++ Labs/4/2/Utils/Utils/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Utils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            int y;
+            Console.WriteLine("Введите первое число:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            y = int.Parse(Console.ReadLine());
+            int greater = Utils.Greater(x, y);
+            Console.WriteLine("Большим из чисел {0} и {1} является {2} ", x, y, greater);
+            Console.WriteLine("До swap: \t" + x + " " + y);
+            Utils.Swap(ref x, ref y);
+            Console.WriteLine("После swap: \t" + x + " " + y);
+        }
+    }
+}
---
+++ Labs/4/2/Utils/Utils/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Utils
+{
+    class Utils
+    {
+        public static int Greater(int a, int b)
+        {
+            if (a > b) return a;
+            else return b;
+        }
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
