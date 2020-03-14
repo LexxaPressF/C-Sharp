0216ba1599d7e553f149f3d741537c392bc93976
LexxaPressF <malyshevalexey6@gmail.com>
2020-03-14 16:04:46 +0300

    5 лаба фулл

---
+++ Labs/5/2 ex/MatrixMultiply/MatrixMultiply/MatrixMultiply.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace MatrixMultiply
+{
+    class MatrixMultiply
+    {
+        static void Vvod(ref double[,] mas) //Функция для ввода массива
+        {
+            int i,j; //Для итерации
+            for(j=0; j<2; j++)//По столбикам
+            {
+                for (i = 0; i < 2; i++)//По рядам
+                {
+                    switch (j)
+                    {
+                        case 0: Console.Write((i + 1) + "-я переменная: "); break;
+                        case 1: Console.Write((i + 3) + "-я переменная: "); break;
+                    }
+                    mas[j,i] = double.Parse(Console.ReadLine());
+                }        
+            }
+        }
+        static void Vyvod(ref double[,] mas) //Функция для вывода массива
+        {
+            int i, j; //Для итерации
+            Console.WriteLine("---Значение массива---");
+            Console.WriteLine("| {0}  {1} |\n| {2}  {3} |", mas[0, 0],
+               mas[0, 1], mas[1, 0], mas[1, 1]);
+        }
+        static void Main()
+        {
+            double[,] firstArray = new double[2, 2], secondArray = new double[2, 2];//Объявление массивов
+            Console.WriteLine("Введите значения 1-го массива:");
+            Vvod(ref firstArray);
+            Console.WriteLine("Введите значения 2-го массива:");
+            Vvod(ref secondArray);
+            Vyvod(ref firstArray);
+            Vyvod(ref secondArray);
+            double[,] result = Multiply(firstArray, secondArray);
+            //Vyvod(ref result);
+            Output(result);
+
+        }
+
+        private static double[,] Multiply(double[,] firstArray, double[,] secondArray)
+        {
+            double[,] result = new double[2, 2];
+            for (int r = 0; r < result.GetLength(0); r++)
+            {
+                for (int c = 0; c < result.GetLength(1); c++)
+                {
+                    result[r, c] += firstArray[r, 0] * secondArray[0, c] + firstArray[r, 1] * secondArray[1, c];
+                }
+            }
+            return result;
+        }
+
+        private static void Output(double[,] result)
+        {
+            Console.WriteLine(result[0, 0]);
+            Console.WriteLine(result[0, 1]);
+            Console.WriteLine(result[1, 0]);
+            Console.WriteLine(result[1, 1]);
+        }
+    }
+}
---
+++ Labs/5/3 ex/Data_processing/Data_processing/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Data_processing
+{
+    class Program
+    {
+        static void Vvod(ref double[] mas) //Функция для ввода массива
+        {
+            Console.WriteLine("Введите массив:");
+            int i; //Для итерации
+            for (i = 0; i < mas.Length; i++)//По столбикам
+            {
+                Console.Write("{0} = ", i);
+                mas[i] = double.Parse(Console.ReadLine());
+            }
+        }
+        static void Vyvod(ref double[] mas) //Функция для вывода массива
+        {
+            Console.WriteLine("------Значения массива------");
+            int i; //Для итерации
+            for (i=0; i < mas.Length; i++)
+            {
+            Console.WriteLine(i + "-й элемент = " + mas[i]);
+            }
+        }
+        static double[] Processing(ref double[] mas) //Обработка массива
+        {                                  //Он возвращает массив найденных значений
+            
+            double s = 0, av = 0, neg = 0, odd=0, max= mas[0], min= mas[0], sm = 1; //Искомые значения
+            double temp1 = 0, temp2 = 0;
+            int i; //Для итерации
+            for (i=0; i<mas.Length; i++)//Цикл для нахождения суммы, кол-ва отрицю и нечетных
+            {
+                s += mas[i];
+                if (mas[i] < 0) neg += mas[i];
+                if ((mas[i] % 2) == 1) odd += mas[i];
+            }
+            for (i = 1; i < mas.Length; i++)
+            {
+                if (mas[i] < min) { min = mas[i]; temp1 = i; }
+                if (mas[i] > max) { max = mas[i]; temp2 = i; }
+            }
+            min = temp1;
+            max = temp2;
+            if (max > min) for (i = (int)min + 1; i < (int)max; i++) sm *= mas[i];
+            else for (i = (int)max + 1; i < (int)min; i++) sm *= mas[i];
+            av = s / mas.Length;
+            double[] pr = new[] { s, av, neg, odd, max, sm}; // Массив для результатов
+            Console.WriteLine("Сумма всех элементов: {0}\nСреднее значение массива: {1}" +
+                "\nСумма отрицательных элементов: {2}\nСумма нечетных элементов: {3}" +
+                "\nНомер максимального элемента: {4}\nПроизведение между максимумом и минимумом: {5}",
+                pr[0], pr[1], pr[2], pr[3], pr[4], pr[5]);
+            return pr;
+        }
+        static void Main()
+        {
+            Console.Write("Введите кол-во элементов в массиве: ");
+            int n = int.Parse(Console.ReadLine());
+            double[] a = new double[n];
+            Vvod(ref a);
+            Vyvod(ref a);
+            Processing(ref a);
+        }
+    }
+}
