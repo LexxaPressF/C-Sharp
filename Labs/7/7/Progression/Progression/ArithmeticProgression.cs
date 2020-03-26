7fc81503acb645e205f6f3215cd2b7f9c557aed6
Алексей Андреевич Малышев <286473@niuitmo.ru>
2020-03-26 18:12:17 +0300

    7 лаьа фулл

---
+++ Labs/7/7/Progression/Progression/ArithmeticProgression.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progression
+{
+    class ArithmeticProgression : Progression
+    {
+        //Свойства
+        public double d; //Разность между элементами
+        public double s; //Сумма прогрессии
+        //Конструкторы
+        public ArithmeticProgression(double a, double b, 
+            double d): base(a,b)
+        {
+            this.d = d;
+            this.s = Summa(this);
+        }
+        public ArithmeticProgression() : base()
+        {
+            Console.Write("Введите разность между элементами: ");
+            double d = double.Parse(Console.ReadLine());
+            this.d = d;
+            this.s = Summa(this);
+        }
+        //Методы
+        new public void Print()
+        {
+            base.Print();
+            Console.WriteLine("Разность между элементами: "
+                + this.d);
+            Console.WriteLine("Сумма элементов: " + this.s);
+            Console.WriteLine("================================");
+        }
+        private double Summa(ArithmeticProgression a)
+        {
+            double s = (2 * a.a0 + a.d*(a.n - 1) )* n / 2;
+            return s;
+        }
+        override public double GetElement(int k)
+        {
+            double a = this.a0 + (k - 1) * this.d;
+            return a;
+        }
+    }
+}
---
+++ Labs/7/7/Progression/Progression/Progression.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progression
+{
+    abstract class Progression
+    {
+        //Свойства
+        public double a0; //Первый элемент
+        public double n; //Количество элементов
+        //Конструкторы
+        public Progression(double a, double b)
+        {
+            this.a0 = a;
+            this.n = b;
+        }
+        public Progression()
+        {
+            Console.Write("Введите первый элемент: ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("Введите кол-во элементов: ");
+            double b = double.Parse(Console.ReadLine());
+            this.a0 = a;
+            this.n = b;
+        }
+        //Методы
+        abstract public double GetElement(int k);
+        virtual public void Print()
+        {
+            Console.WriteLine("================Прогрессия" +
+                "================");
+            Console.WriteLine("Кол-во элементов: " + this.n);
+            Console.WriteLine("Первый элемент: " + this.a0);
+        }
+    }
+}
