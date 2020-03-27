889ecd5e7248e03ee4ee4ffdcfbbbedaeda29ef5
Алексей Андреевич Малышев <286473@niuitmo.ru>
2020-03-27 10:05:21 +0300

    Конец

---
+++ Labs/9/1/Bibl/Bibl/Item.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyClass
+{
+    abstract public class Item : IComparable
+    {
+        //Свойства
+        protected long invNumber; // инвентарный номер — целое число
+        protected bool taken; // хранит состояние объекта - взят ли на руки
+        //Конструкторы
+        public Item(long invNumber, bool taken)
+        {
+            this.invNumber = invNumber;
+            this.taken = taken;
+        }
+        public Item()
+        {
+            this.taken = true;
+        }
+        //Методы
+        //Новый метода сравнения
+        int IComparable.CompareTo(object obj)
+        {
+            Item it = (Item)obj;
+            if (this.invNumber == it.invNumber) return 0;
+            else if (this.invNumber > it.invNumber) return 1;
+            else return -1;
+        }
+        public bool IsAvailable()
+        { // истина, если этот предмет имеется в библиотеке
+            if (taken == true)
+                return true;
+            else
+                return false;
+        }
+        public long GetInvNumber()
+        {// инвентарный номер
+            return invNumber;
+        }
+        public void Take()
+        {// операция "взять"
+            taken = false;
+        }
+        public abstract void Return();//Теперь он абстрактный
+        public virtual void Print()
+        {// Вывод
+            Console.WriteLine("Состояние единицы хранения:\n Инвентарный " +
+                "номер: {0}\n Наличие: {1} ", invNumber, taken);
+        }
+
+    }
+}
